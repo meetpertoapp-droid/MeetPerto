5a06c68e57d10462a6c1554d02f6a3dbd217057a
eliasroberto26-arch <eliasroberto26@gmail.com>
2026-06-28 18:24:56 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAlgnTFVTjbe96J9x13KPROMKWTaCapFFw",
+  authDomain: "meetperto-app-2aad2.firebaseapp.com",
+  projectId: "meetperto-app-2aad2",
+  storageBucket: "meetperto-app-2aad2.firebasestorage.app",
+  messagingSenderId: "99473420964",
+  appId: "1:99473420964:web:80a465eb92a0c6a8f6ec31"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
